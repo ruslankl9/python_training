ef9d11bd862b5167e78010e4b98d6f3ced036108
ruslankl9 <ruslan261991@gmail.com>
2016-10-08 13:21:33 +0500

    Выделен класс для управления сессиями

--- test_add_group.py
+++ test_add_group.py
@@ -12,11 +12,11 @@ def app(request):
     return fixture
     
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="group1", header="4234324", footer="6ergdgdfgg"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
