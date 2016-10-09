53cf9adda38ac081ef3f518025f321c6ca7393a2
ruslankl9 <ruslan261991@gmail.com>
2016-10-09 12:37:31 +0500

    Общая фикстура для всех тестов в файле conftest.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
 from model.contact import Contact
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
